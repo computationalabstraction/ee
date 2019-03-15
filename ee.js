13de8a5a57b7166a9878f3219766872e19cdbf38
archanpatkar <pat@archanpatkar.com>
2019-03-15 08:14:01 +0530

    Update

--- ee.js
+++ ee.js
@@ -1,9 +1,21 @@
 function ee()
 {
-    let map = {};
+    let map = {"*":[]};
+    let regex = [];
     return {
-        emit:(e,...vals)=>map[e].forEach(f => f(...vals)),
-        on:(e,f)=>map[e]?map[e].push(f):(map[e]=[]).push(f)
+        emit:(e,...vals)=>{map[e]?map[e].forEach(f => f(...vals)):0;regex.forEach(o=>e.match(o.p)==null?0:o.f(...vals));e!="*"?map["*"].forEach(f=>f(e,...vals)):0},
+        on:(e,f)=>e instanceof RegExp?regex.push({p:e,f:f}):map[e]?map[e].push(f):(map[e]=[]).push(f),
+        off:(e,f)=>{
+            let o;
+            if(e instanceof RegExp) {
+                for(let i of regex) e.toString()==i.p.toString()&&f==i.f?o=i:0;
+                if(o) delete regex[regex.indexOf(o)];
+            }
+            else if(map[e]) {
+                for(let i of map[e]) f==i?o=i:0;
+                if(o) delete map[e][map[e].indexOf(o)];
+            }
+        }
     };
 } 
 if(typeof(window) === "undefined") module.exports = ee;
