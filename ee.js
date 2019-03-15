471371151757ebc9f2592c08e8deb127a47f340d
archanpatkar <pat@archanpatkar.com>
2019-03-15 08:36:06 +0530

    Update

--- ee.js
+++ ee.js
@@ -5,17 +5,7 @@ function ee()
     return {
         emit:(e,...vals)=>{map[e]?map[e].forEach(f => f(...vals)):0;regex.forEach(o=>e.match(o.p)==null?0:o.f(...vals));e!="*"?map["*"].forEach(f=>f(e,...vals)):0},
         on:(e,f)=>e instanceof RegExp?regex.push({p:e,f:f}):map[e]?map[e].push(f):(map[e]=[]).push(f),
-        off:(e,f)=>{
-            let o;
-            if(e instanceof RegExp) {
-                for(let i of regex) e.toString()==i.p.toString()&&f==i.f?o=i:0;
-                if(o) delete regex[regex.indexOf(o)];
-            }
-            else if(map[e]) {
-                for(let i of map[e]) f==i?o=i:0;
-                if(o) delete map[e][map[e].indexOf(o)];
-            }
-        }
+        off:(e,f)=>{let o;e instanceof RegExp?regex.forEach(i=>e.toString()==i.p.toString()&&f==i.f?o=i:0)||o?delete regex[regex.indexOf(o)]:0:map[e]?map[e].forEach(i=>f==i?o=i:0)||o?delete map[e][map[e].indexOf(o)]:0:0}
     };
-} 
+}
 if(typeof(window) === "undefined") module.exports = ee;
